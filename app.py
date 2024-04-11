92e606c046633f365aa0d78bfad8097235df6cd9
ryankrabbe <161783561+ryankrabbe@users.noreply.github.com>
2024-04-10 20:26:51 -1000

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,76 @@
 # Imports at the top
 import plotly.express as px
-import faicons as fa
-from faicons import icon_svg
-from shiny import reactive, render, req
-from shiny.express import input, render, ui
+from shiny.express import input, ui
+from shinywidgets import render_plotly, render_widget
 from shinyswatch import theme
+from palmerpenguins import load_penguins
+import palmerpenguins
+import seaborn as sns
+from shiny import reactive, render
+from faicons import icon_svg
 
-tips = px.data.tips()
-bill_rng = (min(tips.total_bill), max(tips.total_bill))
+#Load Penguins Dataset
+penguins_df = palmerpenguins.load_penguins()
+
+#Reactive Calc to Filter Data
+@reactive.calc
+def filtered_data():
+    return penguins_df[
+        (penguins_df["species"].isin(input.selected_species_list())) &
+        (penguins_df["island"].isin(input.selected_island_list()))
+    ]
 
 # Page Title
-ui.page_opts(title="Restaurant gratuity in America by Regions", fillable=True)
+ui.page_opts(title="Penguins Dashboard", fillable=True)
 
 # Add a theme
 theme.morph()
 
 #Add a sidebar
-with ui.sidebar():
-    ui.input_slider("total_bill", "Bill Cost", min=bill_rng[0], max=bill_rng[1], value=bill_rng, pre="$")
-    ui.input_checkbox_group("time", "Time of Meal", ["Breakfast", "Lunch"], selected=["Breakfast", "Lunch"], inline=True)
-    ui.input_checkbox_group("sex", "Gender", ["Male", "Female"], selected=["Male", "Female"], inline=True)
-    ui.input_action_button("reset", "Reset filter")
+with ui.sidebar(open="open"):
+    with ui.accordion():
+        with ui.accordion_panel("Select Species", style="background-color: #FFDAB9;"):
+          ui.input_checkbox_group("selected_species_list", 
+                                 "Select Species", 
+                                 ["Adelie", "Gentoo", "Chinstrap"], 
+                                 selected=["Adelie"], 
+                                 inline=True)
+        with ui.accordion_panel("Select Island", style="background-color: #FFDAB9;"):
+          ui.input_checkbox_group("selected_island_list", 
+                                 "Select Island", 
+                                 ["Torgersen", "Biscoe", "Dream"], 
+                                 selected=["Torgersen"], 
+                                 inline=True)
+        with ui.accordion_panel("Select Attribute", style="background-color: #FFDAB9;"):
+            ui.input_selectize("selected_attribute",
+                            "Select Attribute",
+                            ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+                           )
+        with ui.accordion_panel("Plotly Bins", style="background-color: #FFDAB9;"):
+            ui.input_slider("plotly_bin_count", "Plotly bin count", 1, 25, 15)
+        with ui.accordion_panel("Seaborn Bins", style="background-color: #FFDAB9;"):
+            ui.input_slider("seaborn_bin_count", "Seaborn bin count", 1, 25, 15)
+        with ui.accordion_panel("Body Mass", style="background-color: #FFDAB9;"):
+            ui.input_slider("body_mass_count", "Body Mass Range", 2700, 6300, 2700)
+        with ui.accordion_panel("Sex", style="background-color: #FFDAB9;"):
+            ui.input_checkbox_group(
+        "sex",
+        "Sex",
+        ["male", "female"],
+        selected=["male", "female"],
+    )    
 
-#Links
-ui.h6("Links:")
-ui.a("GitHub", href="https://github.com/ryankrabbe/cintel-06-custom", target="_blank", style="color: red;",)
+    ui.hr()
+    ui.a("Github", href="https://github.com/ryankrabbe/cintel-06-custom", target="_blank", style="color: Red;")
 
-ICONS = {
-    "user": fa.icon_svg("user", "regular"),
-    "tag": fa.icon_svg("tag"),
-    "receipt": fa.icon_svg("receipt"),
-}
+    #The main section with according, cards, value boxes, and space for grids and charts
+    with ui.accordion():
+        with ui.accordion_panel("Penguins Dashboard"):
+            with ui.layout_columns():
+                with ui.value_box(showcase=icon_svg("snowman"),max_height="200px", theme="bg-gradient-blue-light-blue"):
+                    "Total Penguins"
+                @render.text
+                def display_penguin_count():
+                    df = filtered_data()
+                    
 
-with ui.layout_column_wrap(fill=False):
-    with ui.value_box(showcase=ICONS["user"]):
-        "Total Customers"
-
-        @render.express
-        def total_tippers():
-            tips.shape[0]
-
-with ui.value_box(showcase=ICONS["tag"]):
-    "Average Gratuity"
-
-    @render.express
-    def average_tip():
-        d = tips
-        if d.shape[0] > 0:
-            perc = d.tip / d.total_bill
-            (f"{perc.mean():.1%}")
-
-with ui.value_box(showcase=ICONS["receipt"]):
-        "Average Check"
-
-        @render.express
-        def average_bill():
-            d = tips
-            if d.shape[0] > 0:
-                bill = d.total_bill.mean()
-                f"${bill:.2f}"
